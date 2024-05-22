2e9e8c9cc7c9eb4b90e385f184d0cd64b2ab1ceb
swagga666 <oloh627@gmail.com>
2024-05-22 20:54:51 +0300

    first commit

---
+++ app/api/config.js
@@ -0,0 +1,8 @@
+// export const BASE_URL = 'http://nikitadobriy.nomoredomainswork.ru'
+export const BASE_URL = "http://localhost:3001/api";
+
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
+}
