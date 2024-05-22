681dcbfbe65be799ad6eecf79545d22a0fbd86ab
swagga666 <oloh627@gmail.com>
2024-05-22 21:05:20 +0300

    updated base_url

--- app/api/config.js
+++ app/api/config.js
@@ -1,5 +1,4 @@
-// export const BASE_URL = 'http://nikitadobriy.nomoredomainswork.ru'
-export const BASE_URL = "http://localhost:3001/api";
+export const BASE_URL = 'http://nikitadobriy.nomoredomainswork.ru'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
